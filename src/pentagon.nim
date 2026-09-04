d423aadabb76d1b9191617fe17346b97c7667e11
Lina Torovoltas <193523522+lina-torovoltas@users.noreply.github.com>
2026-09-04 22:26:16 +0500

    add pentagon in nim

---
+++ src/pentagon.nim
@@ -0,0 +1,23 @@
+import std/random
+import std/os
+
+
+
+randomize()
+
+var progress = 0
+
+
+
+while progress < 100:
+    let limit = min(progress + 20, 100)
+    progress = rand(progress + 1 .. limit)
+  
+    echo "Pentagon test hack tool progress: ", progress, "%"
+  
+    sleep(1000)
+
+if rand(1..30) > 20:
+    echo "Pentagon hack roll: Completed successfully."
+else:
+    echo "Pentagon hack roll: Failed."
